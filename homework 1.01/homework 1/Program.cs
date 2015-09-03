b5d126cf123c1fb18b8527ba0d7946cdbc117911
MaximSBolocan <wolverene1@gmail.com>
2015-09-03 18:49:17 -0400

    some changes in the code

--- homework 1.01/homework 1/Program.cs
+++ homework 1.01/homework 1/Program.cs
@@ -10,7 +10,7 @@ namespace homework_1
     {
         static void Main(string[] args)
         {
-           // int answer;
+            //int answer;
             int userAge;
             int heightFeet;
             bool isCitizen;
@@ -24,59 +24,44 @@ namespace homework_1
             double totalHeightCM;
            
 
-      
-           bool no = false;
+        
+
+
+
             System.Console.Write("What is your age ?");
             userAge = int.Parse(System.Console.ReadLine());
-            ////////
+                       
             System.Console.Write("what is your first name? ");
-            firstName = System.Console.ReadLine();
-            /////////
+            firstName = System.Console.ReadLine(); 
+                       
             System.Console.Write("what is you middle initial ?");
             middleInitial = System.Console.ReadLine();
-            ////////
+            
             System.Console.Write("What is you last name ?");
             lastName = System.Console.ReadLine();
-            ////////////
+            
             System.Console.Write("What is you height Feets only  ");
             heightFeet = int.Parse(System.Console.ReadLine());
-            ////////////
+            
             System.Console.Write("what is you height Inces only  ");
             heightInches = double.Parse(System.Console.ReadLine());
             System.Console.WriteLine("Are you a Citizen?");
-            System.Console.WriteLine("Pres 1 for YES, or 0 for NO");
-            answer = int.Parse(System.Console.ReadLine());
-            isCitizen = (answer + userAge) > userAge;
+            System.Console.WriteLine("true for YES, and false for NO");
+            isCitizen = bool.Parse(System.Console.ReadLine());
+          
             canVote = (18 <= userAge) && isCitizen;
             
 
             
             fullName = "[" +firstName +" "+ middleInitial +" " + lastName+"]";
-           
             totalHeightCM = ((12*heightFeet)+heightInches) *2.54;
-
             System.Console.WriteLine("your age is.." + userAge);
             System.Console.WriteLine("your full name is.." + fullName);
-
             System.Console.WriteLine("your height is .." + totalHeightCM + "cm");
-            if (isCitizen)
-            {
-                System.Console.WriteLine("you are citizen");
-            }
-            else
-            {
-                System.Console.WriteLine("you are NOT citizen");
-            }
-            if (canVote)
-            {
-                System.Console.WriteLine("you can VOTE");
-
-            }
-            else
-            {
-                System.Console.WriteLine("you can NOT VOTE");
-            }
+            System.Console.WriteLine("yourVoting status is   " + canVote);
+            System.Console.WriteLine("if is true you can VOTE, if is false you CAN NOT VOTE ");         
             System.Console.ReadKey(true);
+
             System.Console.ReadKey();
         }
     }
